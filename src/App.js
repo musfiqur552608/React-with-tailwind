63b17bb82ab0bce59f4148f6656cf3cdf5206a40
Your Name <mushfiqur15-10872@diu.edu.bd>
2022-10-04 14:31:30 +0600

    linechart added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Pricing from './components/Pricing/Pricing';
+import AssignmentMarks from './components/AssignmentMarks/AssignmentMarks';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <h1 className='text-4xl font-bold underline'>This is a big big header</h1>
       <p>This is just a paragraph</p>
       <Pricing></Pricing>
+      <AssignmentMarks></AssignmentMarks>
     </div>
   );
 }
